de0c5b6af2782d0ec2d9b0140d288a1c2a5c85c6
Andrew S <andrewsal@google.com>
2022-08-29 10:53:13 -0700

    test33

--- terraform/folders/outputs.tf
+++ terraform/folders/outputs.tf
@@ -2,5 +2,3 @@
 Copyright 2022 Google LLC. This software is provided as-is, without warranty or representation for any use or purpose.
 Your use of it is subject to your agreement with Google.
 */
-
-
