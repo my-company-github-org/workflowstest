139e3afcdb304ae44301c7ccf85806434d6dedab
Andrew S <andrewsal@google.com>
2022-08-28 23:31:19 -0700

    test2

--- terraform/folders/main.tf
+++ terraform/folders/main.tf
@@ -2,7 +2,6 @@
 Copyright 2022 Google LLC. This software is provided as-is, without warranty or representation for any use or purpose.
 Your use of it is subject to your agreement with Google.
 */
-
 module "folders" {
   source  = "../../modules/terraform-google-folders"
 
